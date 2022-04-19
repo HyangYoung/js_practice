3e8d7fd84649cbeb2db27ab6d614d25ea89f1e40
HyangYoung <hyangyoungs@gmail.com>
2022-04-18 21:54:42 -0500

    Datatype

--- main.js
+++ main.js
@@ -1,45 +1,66 @@
-console.log("hello javascript")
-console.log(console)
+// console.log("hello javascript")
+// console.log(console)
+//
+// // var, let, const
+//
+// var name = "scalper" //과거 변수 선언, 글로벌하게 등록되기 때문에 현재는 잘 사용되지 않음.
+//
+// for(var i=0; i<10; i++){
+//     var name = "scalper"
+//     var engName = "codeScalper"
+// }
+//
+// console.log(name, engName) //어디서 호출되었는지 정확히 알기 힘들기 때문에 불편
+// console.log(window) //name 변수 확인가능
+//
+// //let은 값이 빈번하게 변경되어야할때
+//
+// let score = 0;
+// score = 2;
+//
+// console.log(score)
+//
+// //const 값이 변하지 않기 때문에 예측이 가능. 변화가 시도가 되면 에러가 발생.
+//
+// const name = "scalper"
+// name = "codeScalper";
+// console.log(name)
+//
+// //const와 let은 블럭단위임 {} 밖에서는 사용불가
+//
+// //사용불가 예시
+//
+// {
+//     let eng = "scalper"
+// }
+//
+// console.log(eng);
+//
+// //블럭 밖에서 사용된 것은 안으로 타고 내려올 수 있다.
+// console.log(eng);
+// {
+//     let eng = "scalper"
+// }
 
-// var, let, const
-
-var name = "scalper" //과거 변수 선언, 글로벌하게 등록되기 때문에 현재는 잘 사용되지 않음.
-
-for(var i=0; i<10; i++){
-    var name = "scalper"
-    var engName = "codeScalper"
-}
-
-console.log(name, engName) //어디서 호출되었는지 정확히 알기 힘들기 때문에 불편
-console.log(window) //name 변수 확인가능
-
-//let은 값이 빈번하게 변경되어야할때
-
-let score = 0;
-score = 2;
-
-console.log(score)
-
-//const 값이 변하지 않기 때문에 예측이 가능. 변화가 시도가 되면 에러가 발생.
+//Data type: String, Number, boolean, null, undefined, symbol
 
 const name = "scalper"
-name = "codeScalper";
-console.log(name)
+const age = 30;
+const isMale = true;
+const isFemale = false;
+let money = null;
+let girlFriend = undefined;
 
-//const와 let은 블럭단위임 {} 밖에서는 사용불가
-
-//사용불가 예시
-
-{
-    let eng = "scalper"
-}
-
-console.log(eng);
-
-//블럭 밖에서 사용된 것은 안으로 타고 내려올 수 있다.
-console.log(eng);
-{
-    let eng = "scalper"
-}
+console.log(typeof money)
 
 
+console.log("my name is ",name," and I am ",age) //old way
+console.log(`my name is ${name} and I am ${age}`)
+
+const animal = "tiger"
+
+console.log(animal.substring(0,2)) //ti
+
+let fruits = "apple, strawberry, grape, watermelon";
+fruits = fruits.split(", ");
+console.log(fruits[2])
